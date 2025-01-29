83e50cbbfed0421a677ab0d0d880336d3a77ca86
Mohammad Omar <mohaomar495@gmail.com>
2025-01-29 23:34:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-def add_binary(a, b):
-    # your code here
-    total = a + b
-    remainder = []
-    while total > 0:
-        remainder.append(total % 2)
-        total = total // 2
-    return "".join([str(i) for i in remainder[::-1]])
+import numpy as np
+def linear_regression_normal_equation(X: list[list[float]], y: list[float]) -> list[float]:
+	# Your code here, make sure to round
+	X = np.array(X)
+	y = np.array(y)
+	theta = np.linalg.inv(X.T @ X) @ X.T @ y
+	return np.abs(theta.round())
 
-print(add_binary(1, 1))
-print(13 // 2)
+
+X = [[1, 1], [1, 2], [1, 3]]
+y = [1, 2, 3]
+print(linear_regression_normal_equation(X, y))
